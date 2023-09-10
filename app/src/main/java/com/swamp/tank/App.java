324db9189e8fc9dfab04bd252d88aec57cddf0a3
Ji Junkai <junkai.ji@studio.unibo.it>
2023-09-10 13:51:52 +0200

    change file s'position and update Director

--- app/src/main/java/com/swamp/tank/App.java
+++ app/src/main/java/com/swamp/tank/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        
+        Director.getInstance().init(stage);;
     }
 
     public static void main(String[] args) {
--- app/src/main/java/com/swamp/tank/Director.java
+++ app/src/main/java/com/swamp/tank/Director.java
@@ -1,9 +1,17 @@
 package com.swamp.tank;
 
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 /**
  * Director
  */
 public class Director {
+
+    public static final double WIDTH = 960;
+    public static final double HEIGHT = 960;
 
     private static Director instance = new Director();
 
@@ -12,4 +20,16 @@ public class Director {
     public static Director getInstance() {
         return instance;
     }
+
+    public void init(Stage stage) {
+        AnchorPane root = new AnchorPane();
+        stage.setTitle(("Tank Game"));
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.getIcons().add(new Image("/image/logo.png"));
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
