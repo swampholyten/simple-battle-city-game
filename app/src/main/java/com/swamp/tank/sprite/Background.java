5ed774c5294cb9cb4388d70533e9b759f6372440
Ji Junkai <junkai.ji@studio.unibo.it>
2023-09-10 15:23:36 +0200

    add background

---
+++ app/src/main/java/com/swamp/tank/sprite/Background.java
@@ -0,0 +1,11 @@
+package com.swamp.tank.sprite;
+
+import javafx.scene.image.Image;
+
+public class Background extends Sprite{
+
+    public Background() {
+        super(new Image("/image/LevelsBackground.jpg"), 0, 0, 960, 640);
+    }
+    
+}
